89a3c1699e962974c8164fc41ff8e40b071506c9
sghanti <santosh_ghanti@intuit.com>
2023-05-20 21:34:51 +0530

    feat: docker and jib

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,9 +1,5 @@
 package com.example.demo.student;
-
 import lombok.*;
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-
-import javax.persistence.*;
 
 @Getter
 @Setter
@@ -11,24 +7,9 @@ import javax.persistence.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "student", schema="public")
 public class Student {
-    @Id
-    @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            generator = "student_sequence",
-            strategy = GenerationType.SEQUENCE
-    )
     private long id;
-
     private String name;
     private String email;
-
-    @Enumerated(EnumType.STRING)
     private Gender gender;
 }
